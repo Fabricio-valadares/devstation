450371cd59cf6f42d3dc542506176072b2f36200
Wesley <wrodrigo33@gmail.com>
2021-03-08 21:17:30 -0300

    feature: register form

---
+++ src/components/RegisterForm/index.jsx
@@ -0,0 +1,107 @@
+import { useState } from "react";
+import { useHistory } from "react-router";
+
+import { FormContainer } from "./styled";
+
+import { Button, TextField } from "@material-ui/core";
+import { Alert } from "@material-ui/lab";
+
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+import axios from "axios";
+
+const RegisterForm = () => {
+  const [errorMsg, setErrorMsg] = useState(false);
+
+  const history = useHistory();
+  const required = "Campo obrigatório";
+
+  const schema = yup.object().shape({
+    username: yup.string().required(required),
+    password: yup
+      .string()
+      .min(8, "Mínimo de 8 dígitos")
+      .matches(
+        /^((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/,
+        "Senha deve conter ao menos uma letra maiúscula, uma minúscula, um número e um caracter especial!"
+      )
+      .required(required),
+    email: yup.string().email("Email Invalido").required(required),
+  });
+
+  const { handleSubmit, register, errors, reset } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const handleForm = (data) => {
+    axios
+      .post("https://kabit-api.herokuapp.com/users/", data)
+      .then(() => {
+        reset();
+        history.push("/login");
+      })
+      .catch((error) => {
+        setErrorMsg(true);
+        setTimeout(() => {
+          setErrorMsg(false);
+        }, 3000);
+        console.log(error);
+      });
+  };
+
+  return (
+    <form onSubmit={handleSubmit(handleForm)}>
+      {errorMsg && (
+        <Alert severity="error">
+          Seu cadastro falhou, verifique os dados e tente novamente.
+        </Alert>
+      )}
+      <FormContainer>
+        <h1>Registro</h1>
+        <TextField
+          margin="dense"
+          variant="outlined"
+          label="User"
+          name="username"
+          size="small"
+          color="primary"
+          inputRef={register}
+          error={!!errors.username}
+          helperText={errors.username?.message}
+        />
+        <TextField
+          margin="dense"
+          variant="outlined"
+          label="Password"
+          name="password"
+          type="password"
+          size="small"
+          color="primary"
+          inputRef={register}
+          error={!!errors.password}
+          helperText={errors.password?.message}
+        />
+        <TextField
+          margin="dense"
+          variant="outlined"
+          label="Email"
+          name="email"
+          size="small"
+          color="primary"
+          inputRef={register}
+          error={!!errors.email}
+          helperText={errors.email?.message}
+        />
+        <div>
+          <Button type="submit" variant="contained" color="primary">
+            Register
+          </Button>
+        </div>
+      </FormContainer>
+    </form>
+  );
+};
+
+export default RegisterForm;
