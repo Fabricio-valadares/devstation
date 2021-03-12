13f6bc195dc6724672baaece3a30a068c75f316f
Wesley <wrodrigo33@gmail.com>
2021-03-12 15:40:07 -0300

    style: changed register page background color

--- src/pages/Register/styled.js
+++ src/pages/Register/styled.js
@@ -3,7 +3,11 @@ import styled from "styled-components";
 export const RegisterPage = styled.div`
   width: 100%;
   height: 100vh;
-  background-color: var(--soft-primary);
+  background-image: linear-gradient(
+    var(--dark-primary),
+    var(--middle-primary),
+    var(--soft-primary)
+  );
   display: flex;
   align-items: center;
   align-content: center;
