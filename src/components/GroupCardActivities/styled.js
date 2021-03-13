c2ed6f0fcf905458325bf8f84a84835a68c28c52
Wesley <wrodrigo33@gmail.com>
2021-03-13 17:20:51 -0300

    refact: groupUserData

--- src/components/GroupCardActivities/styled.js
+++ src/components/GroupCardActivities/styled.js
@@ -3,14 +3,31 @@ import styled from "styled-components";
 export const CardActivities = styled.div`
   background-color: var(--soft-primary);
   color: white;
-  font-size: 1.3vw;
+  font-size: 1vw;
   width: 50%;
   height: 47vh;
   margin-top: 1rem;
   margin-left: 1rem;
-  overflow-y: auto;
-  /* word-break: break-all; */
   text-align: center;
+  border-radius: 3%;
+
+  h1 {
+    font-size: 1.1rem;
+  }
+
+  overflow-y: auto;
+
+  ::-webkit-scrollbar-track {
+    background-color: var(--soft-primary);
+  }
+  ::-webkit-scrollbar {
+    width: 8px;
+    background: var(--middle-primary);
+  }
+  ::-webkit-scrollbar-thumb {
+    background: #a0a0a0;
+    border-radius: 5px;
+  }
 `;
 
 export const Flex = -styled.div`
@@ -26,5 +43,13 @@ export const WhiteBall = styled.div`
 
 export const ActivityDiv = styled.div`
   display: flex;
-  margin: 1rem;
+  margin: 2rem 0.5rem;
+  font-size: 1rem;
+  figure {
+    margin-right: 0.7rem;
+  }
+  div > h1 {
+    margin-bottom: 0.5rem;
+    font-size: 1rem;
+  }
 `;
